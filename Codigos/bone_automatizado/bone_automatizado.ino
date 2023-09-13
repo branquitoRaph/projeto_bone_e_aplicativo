5ec76261838eec389b26bfc353cf4343bd157d9d
BrenoTonini <brenotoninicosta4@hotmail.com>
2023-09-12 22:41:28 -0300

    bone_automatizado_v2
    mudanças : tempo delay; funções; cálculo temperatura.

--- Codigos/bone_automatizado/bone_automatizado.ino
+++ Codigos/bone_automatizado/bone_automatizado.ino
@@ -1,44 +1,42 @@
 bool cooldown = false; 
 float temp_sensor = 0.0;
-int tempo_ativo = 3000;
-int tempo_espera = 20000;
+int tempo_ativo = 4000;
+int tempo_espera = 25000;
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(2, OUTPUT);
-
-
 }
 
 void loop() {
-  temp_sensor = (analogRead(0) * (5.0/1023)) / 0.01;
+  temp_sensor = map(((analogRead(0) - 20) * 3.04), 0, 1023, -40, 125);
   printTemperatura();
   if (cooldown == true){
-    for (int i = 0; i < (tempo_espera / 100); i++){
-      temp_sensor = (analogRead(0) * (5.0/1023)) / 0.01;
+    for (int i = 0; i < (tempo_espera / 500); i++){
+      temp_sensor = map(((analogRead(0) - 20) * 3.04), 0, 1023, -40, 125);
       printTemperatura();
-      delay(100);
+      delay(1000);
     }
     cooldown = false;
   }
   if (temp_sensor > 34.0 && cooldown == false){
     LigarRele();
   }
-  delay(100);
+  delay(1000);
 }
 
-char LigarRele(){
-  Serial.print("R E F R E S C A N D O!");
+void LigarRele(){
+  Serial.println("R E F R E S C A N D O!");
   digitalWrite(2,HIGH);
   delay(tempo_ativo);
   digitalWrite(2,LOW);
   cooldown = true;
-  Serial.print("Interrompendo a refrescagem.");
+  Serial.println("Interrompendo a refrescagem.");
 }
 
-char printTemperatura(){
+void printTemperatura(){
   Serial.print("Temp:");
   Serial.print(temp_sensor);
   Serial.println("°C");
