6975bd7dd09caaed782e0f32d71cbe3e48a7b5d5
BrenoTonini <brenotoninicosta4@hotmail.com>
2023-11-06 15:40:44 -0300

    codigo_bone_automatizado_v2
    segunda versão do código do arduino para o projeto do boné automatizado. inclui bibliotecas que facilitam o uso do sensor de temperatura infravermelho gy906.

---
+++ Codigos/bone_automatizado/bone_automatizado_v2.ino
@@ -0,0 +1,52 @@
+#include <Wire.h>               //Biblioteca I2C
+#include <SparkFunMLX90614.h>   //Biblioteca SensorInfravermelho-gy906   
+
+IRTherm therm;                  //Objeto do sensor
+
+int rele = 2;
+int tempo_espera = 25000;
+int tempo_ativo = 4000;
+
+float temp_ambiente, temp_objeto;
+bool cooldown = false;
+
+void setup() {
+  Serial.begin(9600);
+  therm.begin();
+  therm.setUnit(TEMP_C);
+
+  pinMode(rele, OUTPUT);
+}
+
+void loop() {
+  if (therm.read()) {
+    temp_objeto = therm.object();
+    temp_ambiente = therm.ambient();
+    PrintInfo();
+
+    if (cooldown == true){
+      for (int i = 0; i < (tempo_espera / 1000); i++){
+        temp_objeto = therm.object();
+        temp_ambiente = therm.ambient();
+        PrintInfo();
+        delay(1000);
+      }
+      cooldown = false;
+    }
+    if (temp_objeto > 35.0){
+      LigarRele();
+    }
+  }
+  delay(1000);
+}
+
+void LigarRele(){
+  digitalWrite(rele, HIGH);
+  delay(tempo_ativo);
+  digitalWrite(rele, LOW);
+  cooldown = true;
+}
+
+void PrintInfo(){
+  Serial.println(String(temp_objeto) + "-" + String(temp_ambiente) + "-" + String(cooldown));
+}
